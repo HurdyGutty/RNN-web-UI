a6f7391a2569ce295f27d400bfe407526f9b3008
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2024-01-16 00:06:06 +0700

    test tailwind

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"embed"
+	"html/template"
+	"io"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -13,18 +15,37 @@ import (
 //go:embed static
 var static embed.FS
 
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func newTemplate() *Template {
+	return &Template{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+}
+
 func main() {
 	e := echo.New()
+	e.Renderer = newTemplate()
 
 	e.Use(middleware.StaticWithConfig(middleware.StaticConfig{
-		Root:       "static",
-		Browse:     true,
-		IgnoreBase: true,
 		HTML5:      true,
+		Root:       "static",
 		Filesystem: http.FS(static),
 	}))
 
 	e.Use(middleware.Logger())
 
-	e.Logger.Fatal(e.Start(":8080"))
+	data := "Hello, World!"
+
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "index", data)
+	})
+
+	e.Logger.Fatal(e.Start(":42069"))
 }
