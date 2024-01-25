707d1827c94b1a9dc5fe9166762db34f6c57450b
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2024-01-25 17:50:03 +0700

    change font families that support utf8

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,9 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Iosevka Aile Iaso", "sans-serif"],
-        mono: ["Iosevka Curly Iaso", "monospace"],
-        serif: ["Iosevka Etoile Iaso", "serif"],
+        sans: ["Arimo", "sans-serif"],
+        mono: ["Cousine", "monospace"],
+        serif: ["Tinos", "serif"],
       },
     },
   },
