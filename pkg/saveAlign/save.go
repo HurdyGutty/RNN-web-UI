6a076b1342e7a6ec9c37152f6fb84800932287e7
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2024-02-03 15:24:56 +0700

    fix offset and saveAtLine

--- pkg/saveAlign/save.go
+++ pkg/saveAlign/save.go
@@ -18,16 +18,19 @@ func offsetLines(data []byte, line int) (int, int) {
 	offset := 0
 	next_line := 0
 	for i, b := range data {
-		if line == 0 {
-			offset = i
-		}
 
 		if b == '\n' {
+
 			line--
+			if line == 0 {
+				offset = i + 1
+			}
+
 			if line == -1 {
-				next_line = i
+				next_line = i + 1
 				break
 			}
+			fmt.Println(line, offset, next_line)
 		}
 	}
 	return offset, next_line
@@ -42,40 +45,34 @@ func saveAtLine(path string, line int, data []byte) {
 
 	offset, next_line := offsetLines(content, line)
 
-	f, err := os.OpenFile("internal/DB/test.txt", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
 	if err != nil {
 		panic(err)
 	}
 	defer f.Close()
+	fmt.Println(offset, next_line)
 
 	end_line := offset + len(data)
-
+	f.WriteAt(content[:offset], 0)
 	f.WriteAt(data, int64(offset))
-	f.WriteAt(content[next_line:], int64(end_line))
+	if next_line != 0 {
+		f.WriteAt(content[next_line:], int64(end_line))
+	} else {
+		f.Truncate(int64(end_line - 1))
+	}
 
 }
 
 func main() {
-	f, err := os.Create("internal/DB/test.txt")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
 
 	data :=
-		[][][][]int{
-			{{{1, 2}, {2}}, {{3}, {1, 4}}, {{5, 4}, {6}}, {{7, 6}, {8}}},
-			{{{0}, {0}}, {{1}, {1}}, {{2}, {2}}, {{3}, {3}}, {{4}, {4}}, {{5}, {5}}, {{6}, {6}}},
-			{{{0}, {1}}, {{1}, {2}}, {{2}, {3}}, {{3}, {4}}, {{4}, {5}}, {{5}, {6}}, {{6}, {7}}},
+		[][][]int{
+			{{1, 2}, {2}}, {{3}, {1, 4}}, {{5, 4}, {6}}, {{7, 6}, {8}},
 		}
 
-	for i := 0; i < len(data); i++ {
-		text := AlignsToBytes(data[i])
-		_, err = f.WriteString(string(text) + "\n")
-		if err != nil {
-			panic(err)
-		}
-	}
+	line := append(AlignsToBytes(data), '\n')
+
+	saveAtLine("internal/DB/test.txt", 0, line)
 
 	fmt.Println("done")
 }
